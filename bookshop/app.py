351d9128a2b563a2274f71b648de2980a5cf09af
Urbiš <M50841@u-ssi.net>
2019-02-19 14:17:39 +0100

    Changed headin in bookshop.

--- bookshop/app.py
+++ bookshop/app.py
@@ -4,11 +4,11 @@ import requests
 
 
 app = Flask(__name__)
-BOOKS = ['1984', 'foo', 'barista', "ahoj a"]
+BOOKS = ['1984', 'foo', 'baristinga', "ahoj a"]
 
 @app.route("/")
 def hello():
-    response = "<H1>Books - Moje knihy:</H1><BR>"
+    response = "<H1>Books - Moje super a bezva knihy:</H1><BR>"
     for book in BOOKS:
         try:
             review = requests.get(f'http://bookreview:8080/v1/review/{book}')
